1776d19aea6247b48f1e0c39a6ad5bb710df816e
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-12-27 00:13:38 +0600

    Added Required Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import Home from './Component/Home/Home'
+import About from './Component/About/About'
+import Contact from './Component/Contact/Contact'
+import ErrorPage from './Component/ErrorPage/ErrorPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +14,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
   )
---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+           <h1 className='syne text-5xl font-bold'>Home</h1>
+           <h1 className='inter text-5xl font-bold'>Home</h1>
+        </div>
+    );
+};
+
+export default Home;
