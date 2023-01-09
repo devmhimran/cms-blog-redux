98980c285cc31bca65f4b1d87e1d374ca3aa1662
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-01-09 22:34:34 +0600

    Added Blog In Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import loadBlogData from '../../Redux/Thunk/loadBlogData';
+import HomeBlog from './HomeBlog';
 
 const Home = () => {
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(loadBlogData())
+    },[])
+    const {blog} = useSelector(state => state.blog)
+    console.log(blog)
     return (
-        <div>
+        <div className='container max-w-screen-xl lg:mx-auto lg:px-0 px-3 py-20'>
             <h1 className='syne text-5xl font-bold'>Home</h1>
-            <h1 className='inter text-5xl font-bold'>Home</h1>
+            <div className="postHub__blog">
+                <div className="grid grid-cols-3 gap-5">
+                    {
+                        blog.map(data => <HomeBlog key={data._id} data={data} />)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
