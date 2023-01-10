b1e8e1261b8893d78e1ddc060f4a1a27d23ade2a
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-01-10 23:52:03 +0600

    Update HomeCategory.jsx

--- src/Component/HomeCategory/HomeCategory.jsx
+++ src/Component/HomeCategory/HomeCategory.jsx
@@ -10,17 +10,18 @@ const HomeCategory = () => {
             <Swiper
                 spaceBetween={50}
                 slidesPerView={3}
+                navigation
                 onSlideChange={() => console.log('slide change')}
                 onSwiper={(swiper) => console.log(swiper)}
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide className='border border-black px-3 p-1'>Slide 1</SwiperSlide>
+                <SwiperSlide className='border border-black px-3 p-1'>Slide 2</SwiperSlide>
+                <SwiperSlide className='border border-black px-3 p-1'>Slide 3</SwiperSlide>
+                <SwiperSlide className='border border-black px-3 p-1'>Slide 4</SwiperSlide>
+                <SwiperSlide className='border border-black px-3 p-1'>Slide 5</SwiperSlide>
+                <SwiperSlide className='border border-black px-3 p-1'>Slide 6</SwiperSlide>
+                <SwiperSlide className='border border-black px-3 p-1'>Slide 7</SwiperSlide>
+                <SwiperSlide className='border border-black px-3 p-1'>Slide 8</SwiperSlide>
             </Swiper>
         </div>
     );
