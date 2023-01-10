b3bca04b97c8f12b86b07fbce7a1bc74a4bf3153
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-01-11 00:23:40 +0600

    Added Pagination In Category Slider

--- src/Component/HomeCategory/HomeCategory.jsx
+++ src/Component/HomeCategory/HomeCategory.jsx
@@ -1,29 +1,32 @@
 import React, { useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
 import { useDispatch, useSelector } from 'react-redux';
 import loadCategoryData from '../../Redux/Thunk/loadCategoryData';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const HomeCategory = () => {
     const dispatch = useDispatch();
     const category = useSelector(state => state.blog.category)
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(loadCategoryData())
-    },[])
+    }, [])
     console.log(category)
     return (
         <div>
             <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={20}
+                slidesPerView={5}
                 navigation
                 onSlideChange={() => console.log('slide change')}
                 onSwiper={(swiper) => console.log(swiper)}
             >
                 {
-                    category.map(data => <SwiperSlide key={data._id} className='bg-gray-300 px-3 p-1 syne'>{data.categoryName}</SwiperSlide>)
+                    category.map(data => <SwiperSlide key={data._id} className='bg-gray-100 p-2 rounded-lg inter text-base font-normal text-center'>{data.categoryName}</SwiperSlide>)
                 }
             </Swiper>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,15 +6,16 @@ import HomeCategory from '../../Component/HomeCategory/HomeCategory';
 
 const Home = () => {
     const dispatch = useDispatch();
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(loadBlogData())
-    },[])
-    const {blog} = useSelector(state => state.blog)
+    }, [])
+    const { blog } = useSelector(state => state.blog)
     console.log(blog)
     return (
-        <div className='container max-w-screen-xl lg:mx-auto lg:px-0 px-3 py-20'>
-            <h1 className='syne text-5xl font-bold'>Home</h1>
-            <HomeCategory/>
+        <div className='container max-w-screen-xl lg:mx-auto lg:px-0 px-3 pb-20'>
+            <div className='py-8'>
+                <HomeCategory />
+            </div>
             <div className="postHub__blog">
                 <div className="grid grid-cols-3 gap-5">
                     {
