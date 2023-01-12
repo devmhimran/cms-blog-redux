e846d44b800460fc4f2434103f7f5e711b32529d
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-01-12 23:38:29 +0600

    Update Flag

--- src/Component/HomeBlog/HomeBlog.jsx
+++ src/Component/HomeBlog/HomeBlog.jsx
@@ -14,7 +14,7 @@ const HomeBlog = ({ data }) => {
                     <h2 className='text-xl syne font-bold'>{blogTitle.slice(0, 38)}...</h2>
                     <div className='flex items-center justify-between'>
                         <p className='my-2 text-base'>Category: <span className='p-1 px-3 border rounded-full'>{blogCategory}</span></p>
-                        <FlagIcon width={'w-6'} />
+                        <FlagIcon width={'w-6'} fill={''} />
                     </div>
                     {/* <p className='inter' dangerouslySetInnerHTML={{__html: content.slice(0,10)}} ></p> */}
                 </div>
