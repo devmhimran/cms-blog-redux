f7315e15fa2ef03a683cea4e226f61f8f94df1c4
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-01-15 23:57:39 +0600

    Added Dynamic Categorize Blog

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,12 +6,28 @@ import HomeCategory from '../../Component/HomeCategory/HomeCategory';
 
 const Home = () => {
     const dispatch = useDispatch();
+    let content;
     useEffect(() => {
         dispatch(loadBlogData())
     }, [])
     const { blog } = useSelector(state => state.blog)
-    console.log(blog)
-    // const {}
+    const { homePageFilter } = useSelector(state => state.filter)
+    // console.log(blog)
+    if(blog.length){
+        content = blog.map(data => <HomeBlog key={data._id} data={data} />)
+    }
+    if(blog.length && homePageFilter){
+        content = blog.filter(data => {
+            if( data ){
+                console.log( data.blogCategory === homePageFilter.filterCategory)
+                // console.log( homePageFilter)
+                return data.blogCategory === homePageFilter.filterCategory
+            }
+            return data
+        })
+        .map(data => <HomeBlog key={data._id} data={data} />)
+    }
+
     return (
         <div className='container max-w-screen-xl lg:mx-auto lg:px-0 px-3 pb-20'>
             <div className='py-8'>
@@ -20,7 +36,7 @@ const Home = () => {
             <div className="postHub__blog">
                 <div className="grid grid-cols-3 gap-5">
                     {
-                        blog.map(data => <HomeBlog key={data._id} data={data} />)
+                        content
                     }
                 </div>
             </div>
