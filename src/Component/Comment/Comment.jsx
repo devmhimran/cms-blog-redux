48efc3ef2a657c4a84eabce2f0aa6c30d25608ea
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-01-28 09:33:31 +0600

    Update Comment Image & Author

--- src/Component/Comment/Comment.jsx
+++ src/Component/Comment/Comment.jsx
@@ -14,29 +14,30 @@ const Comment = ({ data, postAuthor, id }) => {
             .then(data => setProfileUser(data))
     }, [])
 
-    console.log(profileUser)
-    let author = profileUser.filter(profileData => profileData.userId === data.userId)
+    let author = profileUser.filter(profileData => profileData.uid === postAuthor)
+    console.log(author.length)
 
-    
 
     return (
         <div className="comment__data flex gap-2 my-6">
             <div className="profile__images w-[15%]">
                 {
-                    profileUser.filter(profileData => profileData.uid === data.userId).map(data => <img className='w-14 h-14 rounded-full object-cover' src={data.profileImage} alt="" />)
+                    profileUser.filter(profileData => profileData.uid === data.userId).map(data => <img key={data._id} className='w-14 h-14 rounded-full object-cover' src={data.profileImage} alt="" />)
+                    // profileUser.filter(profileData => console.log(profileData))
                 }
                 
             </div>
             <div className="comment__content p-3 bg-slate-100 rounded-lg w-[80%]">
                 <p className='font-semibold'>
                     {
-                        profileUser.filter(data => data.uid === postAuthor).map(data => <span key={data._id} className='font-semibold capitalize'>{data.name}</span>)
+                        profileUser.filter(profileData => profileData.uid === data.userId).map(data => <span key={data._id} className='font-semibold capitalize'>{data.name}</span>)
                     }
                     {/* {
                         profileUser.filter(data => data.uid === user.uid).map(data => <span className='font-semibold capitalize'>{data.name}</span> )
                     } */}
                     
-                    <small className='text-gray-400'>  - { author ? 'Author' : '' }</small>
+                    <small className='text-gray-400'>{ data.userId === postAuthor ? ' - Author' : '' }</small>
+                    {/* <small className='text-gray-400'>  - {  profileUser.map(profileData => profileData.uid === postAuthor) ? 'author' : '' }</small> */}
                 </p>
                 <p className='inter'>{data.blogComment}</p>
             </div>
